6c7322fbf479df5c839363ceb1846fa86048eced
Jimklean <109154325+Jimklean@users.noreply.github.com>
2022-07-12 14:42:31 +0100

    Add files via upload

---
+++ footer/index.php
@@ -0,0 +1,138 @@
+<footer id="footer" class="dark">
+
+    <div class="container" id="contact">
+
+        <!-- Footer Widgets
+				============================================= -->
+        <div class="footer-widgets-wrap clearfix">
+
+            <div class="col_half">
+
+                <div class="col_half">
+                    <div class="row">
+                        <div class="widget clearfix">
+
+                            <img src="images/logo-dark.png" alt="" class="footer-logo" style="margin-left: -25px;">
+
+                            <p>Professional <strong>Facility</strong> Maintenance Service</p>
+
+
+
+                        </div>
+                    </div>
+                    <div class="row">
+                        Company Reg No.: <strong>13137752</strong>
+                    </div>
+                </div>
+
+
+                <div class="col_half col_last">
+
+                    <div class="widget widget_links clearfix">
+                        <h4>Useful Downloads</h4>
+
+                        <div style="background: url('images/world-map.png') no-repeat center center; background-size: contain;">
+                            <ul>
+                                <li><a href="images/pdf/termsANDconditions-Jimklean.pdf" target="_blank"><small style="display: block; margin-top: 3px;"><strong>Terms</strong> and Conditions</small></a></li>
+                                <li><a href="images/pdf/Jimklean-client-handbook.pdf" target="_blank"><small style="display: block; margin-top: 3px;">Client Handbook</small></a></li>
+                                <li><a href="images/pdf/Service-Payment-Agreement.pdf" target="_blank"><small style="display: block; margin-top: 3px;">Service Payment Form</small></a></li>
+                                <li><a href="images/pdf/Staff-Handbook-jimklean.docx.pdf" target="_blank"><small style="display: block; margin-top: 3px;">Staff Handbook</small></a></li>
+                                <li><a href="images/pdf/Jimkleans-Coronovirus.pdf" target="_blank"><small style="display: block; margin-top: 3px;">COVID-19 Guide</small></a></li>
+                            </ul>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="col_half col_last">
+
+                <div class="widget clearfix" style="margin-bottom: -20px;">
+                    <h4>Contact Us</h4>
+
+                </div>
+
+                <div style="" class="widget subscribe-widget clearfix">
+                    <div class="col_half ">
+                        <address>
+                            <strong>Headquarters:</strong><br>
+                            29 Manley street, Salford, M7 2FJ<br>
+                            <ul class="iconlist nomargin">
+                                <li><i class="icon-time color"></i> <strong>Mondays - Saturdays:</strong> 9:00 – 17:30</li>
+                                <li><i class="icon-time color"></i> <strong>Sundays:</strong> 10:00 – 16:00</li>
+                            </ul>
+                        </address>
+                    </div>
+                    <div class="col_half col_last"><abbr title="Phone Number"><strong>Phone:</strong></abbr> 07449359449<br>
+                        <abbr title="Fax"><strong>Fax:</strong></abbr> 01614255319<br>
+                        <abbr title="Email Address"><strong>Email:</strong></abbr> info@jimkleanservice.com
+                    </div>
+                </div>
+
+                <div class="widget clearfix" style="margin: 0px;">
+
+                    <div class="row">
+
+                        <div class="col-lg-4 clearfix bottommargin-sm">
+                            <a href="https://www.facebook.com/JimKlean-Services-100723312334966/" class="social-icon si-dark si-colored si-facebook nobottommargin" style="margin-right: 10px;" target="_blank">
+                                <i class="icon-facebook"></i>
+                                <i class="icon-facebook"></i>
+                            </a>
+
+                            <a href="https://www.facebook.com/JimKlean-Services-100723312334966/" target="_blank"><small style="display: block; margin-top: 3px;"><strong>Like us</strong><br>on Facebook</small></a>
+                        </div>
+                        <div class="col-lg-4 clearfix bottommargin-sm">
+                            <a href="https://www.instagram.com/jimklean_services/" target="_blank" class="social-icon si-dark si-colored si-yahoo nobottommargin" style="margin-right: 10px;">
+                                <i class="icon-instagram"></i>
+                                <i class="icon-instagram"></i>
+                            </a>
+
+                            <a href="https://www.instagram.com/jimklean_services/" target="_blank"><small style="display: block; margin-top: 3px;"><strong>Follow us</strong><br>on Instagram</small></a>
+                        </div>
+
+                        <div class="col-lg-4 clearfix bottommargin-sm">
+                            <a href="https://twitter.com/jimklean_s" target="_blank" class="social-icon si-dark si-colored si-twitter nobottommargin" style="margin-right: 10px;">
+                                <i class="icon-twitter"></i>
+                                <i class="icon-twitter"></i>
+                            </a>
+
+                            <a href="https://twitter.com/jimklean_s" target="_blank"><small style="display: block; margin-top: 3px;"><strong>Follow us</strong><br>on Twitter</small></a>
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div><!-- .footer-widgets-wrap end -->
+
+    </div>
+
+    <!-- Copyrights
+			============================================= -->
+    <div id="copyrights">
+
+        <div class="container clearfix">
+
+            <div class="col_half">
+                Jimklean Services&copy; <script>
+                    document.write(new Date().getFullYear())
+                </script> All Rights Reserved<br><br>
+                <div class="copyright-links"><a href="images/pdf/jimklean-privacy-notice.docx.pdf">Privacy Policy</a> / <a href="#" id="open_preferences_center">Change your preferences</a></div>
+            </div>
+
+            <div class="col_half col_last tright">
+
+
+                <div class="clear"></div>
+
+                <i class="icon-heart"></i> Built By <a href="https://www.zine.digital" target="_blank">the Zine</a>
+            </div>
+
+        </div>
+
+    </div><!-- #copyrights end -->
+
+</footer><!-- #footer end -->
